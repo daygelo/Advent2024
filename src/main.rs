a21452462268ebef9dde166f50b7c1fef20a487b
Angelo Djeumene <80226633+daygelo@users.noreply.github.com>
2024-12-09 18:19:04 -0600

    Day 9 Part 1

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-mod day8;
+mod day9;
 
 fn main() {
-    let input = std::fs::read_to_string("src/day8/input.txt").unwrap();
-    println!("{} {}", day8::part1(&input), day8::part2(&input));
+    let input = std::fs::read_to_string("src/day9/input.txt").unwrap();
+    println!("{} {}", day9::part1(&input), day9::part2(&input));
 }
